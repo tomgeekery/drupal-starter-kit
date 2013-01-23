5571a8e0cdde8517d61e66881e3e55437da70190
Chris Keller <chris@commercialprogression.com>
2013-01-22 20:33:23 -0500

    Initial commit.

---
+++ create_drupal_site.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+if [ -z $1 ]; then
+	echo "Usage: create_drupal_site.sh newprojectname"
+	exit 0
+fi
+
+NAME=$1
+
+cd ~/htdocs
+drush make https://raw.github.com/tomgeekery/compro/master/make/compro.make $NAME
+
+cd $NAME
+
+mkdir sites/default/files
+chmod 777 sites/default/files
+
+cp sites/default/default.settings.php sites/default/settings.php
+chmod 777 sites/default/settings.php
+
+PASS=${NAME//o/0}
+PASS=${PASS//i/1}
+PASS=${PASS//e/3}
+PASS=${PASS//a/@}
+
+read -s -p "Enter your MYSQL password: " SQLPASS
+mysql -uroot -p$SQLPASS -e "create database $NAME"
+mysql -uroot -p$SQLPASS -e "grant all on $NAME.* to $NAME@localhost identified by '$PASS'"
+
+drush site-install compro --db-url=mysql://$NAME:$PASS@localhost/$NAME --account-name=maintenance --account-pass=$PASS --site-name=$NAME
+
+APACHE=/etc/apache2/sites-available
+
+sudo touch $APACHE/$NAME
+
+echo Wrote the following to $APACHE/$NAME
+echo "<VirtualHost *:80>" | sudo tee -a $APACHE/$NAME
+echo "        ServerName "$NAME.local | sudo tee -a $APACHE/$NAME
+echo "        ServerAlias *."$NAME.local | sudo tee -a $APACHE/$NAME
+echo "        DirectoryIndex index.php index.html" | sudo tee -a $APACHE/$NAME
+echo "        DocumentRoot /home/"$USER"/htdocs/"$NAME | sudo tee -a $APACHE/$NAME
+echo "        <Directory /home/"$USER"/htdocs/"$NAME">" | sudo tee -a $APACHE/$NAME
+echo "                Options -Indexes FollowSymLinks" | sudo tee -a $APACHE/$NAME
+echo "                AllowOverride All" | sudo tee -a $APACHE/$NAME
+echo "                Order allow,deny" | sudo tee -a $APACHE/$NAME
+echo "                allow from all" | sudo tee -a $APACHE/$NAME
+echo "        </Directory>" | sudo tee -a $APACHE/$NAME
+echo "        ErrorLog /var/log/apache2/"$NAME.local_error.log | sudo tee -a $APACHE/$NAME
+echo "        CustomLog /var/log/apache2/"$NAME.local_access.log combined | sudo tee -a $APACHE/$NAME
+echo "</VirtualHost>" | sudo tee -a $APACHE/$NAME
+
+echo Activating site...
+sudo a2ensite $NAME
+echo Done.
+
+echo Restarting apache2...
+sudo service apache2 restart
+echo Done.
+
+echo Adding vhost entry to hosts file...
+echo 127.0.0.1"       "$NAME.local | sudo tee -a /etc/hosts
+echo Done.
+
+git config --global core.excludesfile ~/.gitignore_global
+git init
+git add .
+git commit -m "Initial commit."
+
+echo Visit the new site @ http://$NAME.local
+echo Username: maintenance
+echo Password: $PASS
+
+chmod 444 sites/default/settings.php
+
+exit 0
---
+++ environment_setup.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+# Script is intended to take a fresh install of Ubuntu 12.04 and prepare
+# it for local Drupal development.
+
+# Update all packages.
+sudo apt-get update
+sudo apt-get upgrade -y
+
+# Install lamp stack.
+sudo apt-get install lamp-server^ -y
+
+# Install extra php and misc packages.
+sudo apt-get install php5-gd php-pear make git phpmyadmin -y
+
+# Upload progress
+sudo pecl install uploadprogress
+sudo sh -c "echo 'extension = uploadprogress.so' > /etc/php5/apache2/conf.d/uploadprogress.ini"
+
+# Drush
+sudo pear channel-discover pear.drush.org
+sudo pear install drush/drush
+wget http://download.pear.php.net/package/Console_Table-1.1.3.tgz
+tar xvfz Console_Table-1.1.3.tgz
+sudo mv Console_Table-1.1.3 /usr/share/php/drush/lib
+rm Console_Table-1.1.3.tgz
+rm package.xml
+
+# Enable rewrite
+sudo a2enmod rewrite
+
+# Setup htdocs in home folder
+mkdir /home/$USER/htdocs
+sudo ln -s /home/$USER/htdocs /var/www/$USER
+
+# Download development script(s).
+cd ~/htdocs
+git clone https://github.com/tomgeekery/drupal-in-a-can
+cp ~/htdocs/drupal-in-a-can/.gitignore_global ~
+echo -n "Enter your name for git commits: "
+read GIT_NAME
+echo -n "Enter your email for git commits: "
+read GIT_EMAIL
+git config --global user.name "'"$GIT_NAME"'"
+git config --global user.email $GIT_EMAIL 
+git config --global core.excludesfile ~/.gitignore_global
+
+# Restart apache.
+sudo service apache2 restart
+
+# Install Geany for basic code editing.
+sudo apt-get install geany -y
+
+# Display message with instructions what to do next.
+echo ""
+echo "Congrats!  Your Ubuntu install is ready for Drupal development."
+echo "Please run './create_drupal_site.sh sitename'.  Replace"
+echo "sitename with the name you wish to call your new Drupal site."
+echo ""
+echo "Enjoy!"
